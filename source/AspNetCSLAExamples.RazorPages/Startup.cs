2a7f3fde1e05ecbf88dc69daaf594c0f53c82d8e
Ayaz Duru <ayzdru@outlook.com>
2020-08-21 17:11:23 +0300

    Yorum satırları kaldırıldı.

--- source/AspNetCSLAExamples.RazorPages/Startup.cs
+++ source/AspNetCSLAExamples.RazorPages/Startup.cs
@@ -23,8 +23,6 @@ namespace AspNetCSLAExamples.RazorPages
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -35,8 +33,6 @@ namespace AspNetCSLAExamples.RazorPages
             services.AddCsla();
             services.AddTransient(typeof(IPersonRepository), typeof(PersonRepository));
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
